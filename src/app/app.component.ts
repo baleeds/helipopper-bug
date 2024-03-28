cdf8323de9732baebc75320212512afdd4da1597
Benjamin Leeds <bleeds@samaritan.org>
2024-03-28 15:26:30 -0400

    add: Minimum example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TippyDirective} from "@ngneat/helipopper";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TippyDirective],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'helipopper-bug';
+  public useHostWidth = false;
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,22 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {popperVariation, provideTippyConfig} from "@ngneat/helipopper";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideTippyConfig({
+      defaultVariation: 'menu',
+      variations: {
+        menu: {
+          ...popperVariation,
+          maxWidth: 600,
+          theme: 'ik-menu',
+          interactive: true,
+          offset: [0, 0],
+        },
+      },
+    }),
+  ],
 };
